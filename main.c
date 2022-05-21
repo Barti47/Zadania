a5e61a167122632b4517d6d3a9107f28c66a2bd1
BartoszWrombel <93789312+BartoszWrombel@users.noreply.github.com>
2022-05-21 11:57:01 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int tablicanxm(int n, int m);
+
+int main()
+{
+    int n=0,m=0;
+    printf("Podaj wymiary tablicy n x m\n");
+    while((m<=0) && (n<=0))
+    {
+        scanf("%d %d",&n,&m);
+    }
+    printf("%d, %d",n,m);//only int number not working
+    tablicanxm(n,m);
+
+    return 0;
+}
+int tablicanxm(int n, int m)
+{
+
+    int **matrix=(int **) malloc(n*sizeof(int *));
+    matrix[0]=(int *)malloc(n*m*sizeof(int));
+    for(int i=1; i<n; i++)
+        matrix[i]=matrix[0]+i*n;
+
+
+}
